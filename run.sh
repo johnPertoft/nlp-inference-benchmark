ae7ea833baf5828f85d2bcab23bf6d22faa64131
John Pertoft <john.pertoft@gmail.com>
2022-12-01 14:02:12 +0000

    Add common run script.

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+RUN_DIR="$1"
+if [ ! -d "$RUN_DIR" ]; then
+    echo "$RUN_DIR is not a directory"
+    exit 1
+fi
+
+pushd "$RUN_DIR"
+docker build -t "$RUN_DIR" .
+docker run -it --rm -v $(pwd)/.cache:/home/vscode/.cache --gpus all "$RUN_DIR"
